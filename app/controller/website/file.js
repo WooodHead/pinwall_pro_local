47912037dc1d1724a8350f74d341e74b632c1579
codeliling <maximuslee@126.com>
2019-04-10 11:39:43 +0800

    增加文件名返回

--- app/controller/website/file.js
+++ app/controller/website/file.js
@@ -67,7 +67,8 @@ class FileController extends BaseController {
         }
         //文件响应
         ctx.body = {
-            url: path.join(ctx.helper.baseUrl, fileTagget.replace(ctx.helper.basePath,''), filename)
+            url: path.join(ctx.helper.baseUrl, fileTagget.replace(ctx.helper.basePath,''), filename),
+            fileNmae:filename
         };
     }
 }
